a91d179b7d17ce0f565da96ba13658144372ac8b
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-27 21:26:38 -0400

    pretty sure I just made a mess

---
+++ script.js
@@ -0,0 +1,12 @@
+let formSubmission = document.getElementById("submission");
+let firstName = document.getElementById("fname");
+let lastName = document.getElementById("lname");
+let pizzaSize = document.getElementById("pizza size");
+let toppingPep = document.getElementById("pepperoni");
+let toppingMush = document.getElementById("mushrooms");
+let toppingCheese = document.getElementById("cheese");
+let deliveryInstruct = document.getElementById("inputBox");
+
+formSubmission.addEventListener('click', function(){
+    
+});
